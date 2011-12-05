34780c0f592e43165c0ffc80e350bbb8b12bc522
Petr Pilař <email@palmik.net>
2011-12-05 21:20:48 +0100

    Trying to track down the invalid reads

--- connection/tests/main.cpp
+++ connection/tests/main.cpp
@@ -1,4 +1,5 @@
 #include <connection/socket/tcp.hpp>
+#include <boost/array.hpp>
 #include <iostream>
 using namespace std;
 using namespace connection;
@@ -13,10 +14,28 @@ void handle_disconnected()
     cout << "Disconnected, noo!" << endl;
 }
 
+void handle_read(tcp::socket& s)
+{
+    cout << "Read ready!" << endl;
+}
+
+void test(boost::array<char, 128>& buf)
+{
+std::cout.write(buf.data(), 128);
+}
+
 int main()
 {
     tcp::socket c;
     c.sig_connected().connect(handle_connected);
     c.sig_disconnected().connect(handle_disconnected);
-    c.connect("ntp.cesnet.cz", "daytime");
+    c.sig_read_ready().connect(boost::bind(handle_read, boost::ref(c)));
+    c.open("ntp.cesnet.cz", "daytime");
+
+
+//       boost::array<char, 128> buf;
+//       boost::system::error_code error;
+// 
+//       c.async_read_some(boost::asio::buffer(buf), boost::bind(test, ref(buf)));
+
 }
